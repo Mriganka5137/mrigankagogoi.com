c3dbaf4782e43769a901fc7e77d260457cb41472
Mriganka Mousum Gogoi <gogoimriganka09@gmail.com>
2023-12-08 13:45:37 +0530

    Update font imports in layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,17 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Poppins } from "next/font/google";
 import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { cn } from "@/lib/utils";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
-import { Poppins } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
-export const spaceGrotesk = Space_Grotesk({
+const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
   variable: "--font-spaceGrotesk",
   display: "swap",
-});
-
-export const poppins = Poppins({
-  weight: ["300", "400", "500", "600", "700"],
-  subsets: ["latin"],
-  variable: "--font--poppins",
 });
 
 export const metadata: Metadata = {
